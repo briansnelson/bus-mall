4f811f2d37593e0e6a3ed2864f8bfcff1c9d303b
briansnelson <brian@grit360.com>
2021-05-29 16:17:23 -0700

    doing the js

---
+++ js/app.js
@@ -0,0 +1,124 @@
+/* eslint-disable new-cap */
+'use strict';
+
+//global variables
+let allProducts = [];
+let clicks = 0;
+let clicksAllowed = 25;
+let productValidation = [];
+
+
+let myContainer = document.querySelector('section');
+let myButton = document.querySelector('div');
+let imageOne = document.getElementById('imageOne');
+let imageTwo = document.getElementById('imageTwo');
+// eslint-disable-next-line no-unused-vars
+let imageThree = document.getElementById('imageThree');
+
+
+
+function makeProduct(product, fileExtension = 'jpg') {
+  this.product = product;
+  this.src = `img/${product}.${fileExtension}`;
+  this.clicks = 0;
+  this.views = 0;
+  allProducts.push(this);
+}
+
+// eslint-disable-next-line new-cap
+new makeProduct('bag');
+new makeProduct('banana');
+new makeProduct('bathroom');
+new makeProduct('boots');
+new makeProduct('breakfast');
+new makeProduct('bubblegum');
+new makeProduct('chair');
+new makeProduct('cthulhu');
+new makeProduct('dog-duck');
+new makeProduct('dragon');
+new makeProduct('pen');
+new makeProduct('pet-sweep');
+new makeProduct('scissors');
+new makeProduct('shark');
+new makeProduct('sweep', 'png');
+new makeProduct('tauntaun');
+new makeProduct('unicorn');
+new makeProduct('water-can');
+new makeProduct('wine-glass');
+
+
+// some validation
+// imageOne.src = allGoats[0].src;
+// imageTwo.src = allGoats[1].src;
+// allGoats[0].views++;
+
+function selectRandomProductIndex() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+function renderRandomProducts() {
+  let productOne = selectRandomProductIndex();
+  let productTwo = selectRandomProductIndex();
+  let productThree = selectRandomProductIndex();
+  // seriously consider using an array. 
+  // remember:  how do you know if an array inculdes something? maybe google
+  while (productValidation.length < 3) {
+    let uniqueProduct = selectRandomProductIndex();
+    while (!productValidation.includes(uniqueProduct)) {
+      productValidation.push(uniqueProduct);
+    }
+  }
+  console.log(productValidation);
+
+  imageOne.src = allProducts[productOne].src;
+  imageOne.alt = allProducts[productOne].name;
+  allProducts[productOne].views++;
+
+  imageTwo.src = allProducts[productTwo].src;
+  imageTwo.alt = allProducts[productTwo].name;
+  allProducts[productTwo].views++;
+
+  imageThree.src = allProducts[productThree].src;
+  imageThree.alt = allProducts[productThree].name;
+  allProducts[productThree].views++;
+}
+
+function handleProductClick(event){
+  if(event.target === myContainer){
+    alert('click on an IMAGE please');
+  }
+
+  clicks++;
+  let clickedGoat = event.target.alt;
+  for (let i = 0; i < allGoats.length; i++){
+    if (clickedGoat === allGoats[i].name){
+      allGoats[i].clicks++;
+    }
+  }
+  renderRandomGoats();
+
+  if(clicks === clicksAllowed){
+    myContainer.removeEventListener('click', handleProductClick);
+  }
+}
+
+function renderResults(){
+  let ul = document.querySelector('ul');
+  for(let i = 0; i < allGoats.length; i++){
+    let li = document.createElement('li');
+    li.textContent = `${allGoats[i].name} had ${allGoats[i].views} views and was clicked ${allGoats[i].clicks} times.`;
+    ul.appendChild(li);
+  }
+}
+
+function handleButtonClick(event){ //eslint-disable-line
+  if(clicks === clicksAllowed){
+    renderResults();
+  }
+}
+
+renderRandomGoats();
+
+
+myContainer.addEventListener('click', handleProductClick);
+myButton.addEventListener('click', handleButtonClick);

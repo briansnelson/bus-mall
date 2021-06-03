d0f5a97306f68a8db94d93d77e49035a693b6338
briansnelson <brian@grit360.com>
2021-06-02 21:40:58 -0700

    pulled in the chart scripts and code, ended up commenting it all out because images would no longer show

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let allProducts = [];
 let clicks = 0;
 let clicksAllowed = 25;
 let productValidation = [];
+let renderLineUp = [];
 
 
 let myContainer = document.querySelector('section');
@@ -62,7 +63,7 @@ function renderRandomProducts() {
   let productThree = selectRandomProductIndex();
   // seriously consider using an array. 
   // remember:  how do you know if an array inculdes something? maybe google
-  while (productValidation.length < 3) {
+  while (productValidation.length < 6) {
     let uniqueProduct = selectRandomProductIndex();
     while (!productValidation.includes(uniqueProduct)) {
       productValidation.push(uniqueProduct);
@@ -116,9 +117,54 @@ function handleButtonClick(event){ //eslint-disable-line
     renderResults();
   }
 }
+// function renderChart() {
+//   let clicksArray = [];
+//   let viewsArray = [];
+//   let productsArray = [];
+
+//   for (let i = 0; i <allProducts.length; i++){
+//     clicksArray.push(allProducts[i].clicks);
+//     viewsArray.push(allProducts[i].views);
+//     productsArray.push(allProducts[i].product);
+//   }
+// console.log(clicksarray)
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
 
 renderRandomProducts();
-
 
 myContainer.addEventListener('click', handleProductClick);
 myButton.addEventListener('click', handleButtonClick);
